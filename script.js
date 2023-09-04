4ce036803c07c63f1718e36bebcfa011d50200fe
Ankitfactech <98767716+Ankitfactech@users.noreply.github.com>
2023-09-04 12:45:00 +0530

    hello

---
+++ script.js
@@ -0,0 +1,28 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+  
+  // const audio = new Audio("WhatsApp Audio 2023-09-04 at 00.04.49.mp4");
+  // const buttons = document.querySelectorAll("button");
+  
+  
+  // buttons.forEach(button => {
+  // button.addEventListener("click", () => {
+    
+  // audio.play();
+  // });
+  // });
+  const audio = new Audio("WhatsApp Audio 2023-09-04 at 00.04.49.mp4");
+  const buttons = document.querySelectorAll(".btx");
+  
+  buttons.forEach(button => {
+    button.addEventListener("click", () => {
+      audio.play();
+    });
+  });
+  
+  
+  
